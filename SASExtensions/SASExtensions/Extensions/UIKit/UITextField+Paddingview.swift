c320cfcb506e8cf39cd5b1000004d264fa3a354e
Ile$h <ilesh.panchal1991@gmail.com>
2019-09-30 15:37:00 +0530

    Update UITextField+Paddingview.swift

--- SASExtensions/SASExtensions/Extensions/UIKit/UITextField+Paddingview.swift
+++ SASExtensions/SASExtensions/Extensions/UIKit/UITextField+Paddingview.swift
@@ -10,6 +10,45 @@ import UIKit
 extension UITextField {
     
     /**
+     @brief Add buttons on right side in the textfield for the password
+     @param  img: UIImage for the normal image
+     @param  selected: UIImage for the selected image
+     `````
+     @remark This is super easy methods
+     
+     */
+    func addPasswordShowButton(img:UIImage,selected:UIImage){
+        if let rightButton = self.rightView, rightButton is UIButton {
+            print("Button exist")
+            return
+        }
+        let button = UIButton(type: .custom)
+        button.setImage(img, for: .normal)
+        button.setImage(selected, for: .selected)
+        //button.backgroundColor = UIColor.red
+        //button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        button.frame = CGRect(x: CGFloat(self.frame.size.width - self.height), y: CGFloat(5), width: self.height, height: self.height)
+        button.removeTarget(self, action: nil, for: .allEvents)
+        button.addTarget(self, action: #selector(secureMode(_:)), for: .touchUpInside)
+        self.rightView = button
+        self.rightView?.bringSubviewToFront(self)
+        self.rightViewMode = .always
+    }
+    
+    @objc func secureMode(_ sender: UIButton) {
+        print("textfield right button")
+        DispatchQueue.main.async {
+            if sender.isSelected {
+                sender.isSelected = false
+                self.isSecureTextEntry = true
+            }else{
+                sender.isSelected = true
+                self.isSecureTextEntry = false
+            }
+        }
+    }
+    
+    /**
      @brief This is Done button in keyboard toolbar
      @param no need paramer
      `````
